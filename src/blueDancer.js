25a33bb10da38b476b728d5b22def8b0a6217f34
jakehsiao0909 <jakehsiao0909@gmail.com>
2018-03-12 17:23:04 -0500

    ayee

---
+++ src/blueDancer.js
@@ -0,0 +1,10 @@
+// var blueDancer = function(top, left, timeBetweenSteps) {
+//     dancer.call(this);
+// };
+
+// blueDancer.prototype = Object.create(dancer.prototype);
+// blueDancer.prototype.constructor = blueDancer;
+
+// blueDancer.prototype.set = function() {
+
+// };
